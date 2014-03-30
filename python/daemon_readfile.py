7c200c74c1250b8129cd39e9048807cb785fa071
sco93 <pscorso93@gmail.com>
2014-03-30 17:08:56 +0200

    added a deamon that continuosly read a file and stores his history in a list of tuples

---
+++ python/daemon_readfile.py
@@ -0,0 +1,32 @@
+import sys
+import time
+import matplotlib.pyplot as plt
+
+
+def watch_file_mods(file=sys.argv[1]):
+    file_history = []
+    try:
+        while True:
+            try:
+                with open(sys.argv[1]) as file:
+                    line = tuple(file.readlines())
+                    try:
+                        if line != file_history[-1][0]:
+                            file_history.append((line, "".join(time.ctime().split(" ")[-2].split(":"))))
+                            print(file_history[-1][0])
+                    except IndexError:
+                        file_history.append((line, "".join(time.ctime().split(" ")[-2].split(":"))))
+                        print(file_history[-1][0])
+            except IOError:
+                pass
+    except KeyboardInterrupt:
+        return file_history
+
+if __name__ == "__main__":
+    file_history = watch_file_mods(sys.argv[1])
+    y = [len(y[0]) for y in file_history]
+    x = [int(x[1]) for x in file_history]
+    print(zip(x, y))
+    plt.plot(x, y, "o")
+    plt.plot(x, y)
+    plt.show()
