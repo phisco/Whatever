2c4fd4c47ea67c46c53223d65c7bec5490cada3d
sco93 <pscorso93@gmail.com>
2014-05-20 15:08:47 +0200

    update

--- python/daemon_readfile.py
+++ python/daemon_readfile.py
@@ -3,13 +3,13 @@ import time
 import matplotlib.pyplot as plt
 
 
-def watch_file_mods(file=sys.argv[1]):
+def watch_file_mods(file_to_watch):
     file_history = []
     try:
         while True:
             try:
-                with open(sys.argv[1]) as file:
-                    line = tuple(file.readlines())
+                with open(file_to_watch) as opened_file:
+                    line = tuple(opened_file.readlines())
                     try:
                         if line != file_history[-1][0]:
                             file_history.append((line, "".join(time.ctime().split(" ")[-2].split(":"))))
@@ -23,11 +23,14 @@ def watch_file_mods(file=sys.argv[1]):
     except KeyboardInterrupt:
         return file_history
 
+
+# def main(file_to_read):
 if __name__ == "__main__":
-    file_history = watch_file_mods(sys.argv[1])
+    file_history = watch_file_mods("test.txt")
+    plt.ion()
     y = [len(y[0]) for y in file_history]
     x = [int(x[1]) for x in file_history]
-    x = map(lambda el: el - x[0], x)
+    x = [el - x[0] for el in x]
     plt.plot(x, y, "o")
     plt.plot(x, y)
     plt.show()
